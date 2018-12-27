60608bc5f479e8fa7c4ba0a79c9c35ef8a93e3fc
aleoltus <aleoltus@gmail.com>
2018-12-27 20:16:33 +0100

    try catch

--- Odczyttab.java
+++ Odczyttab.java
@@ -4,9 +4,18 @@ public class Odczyttab {
     public static void main(String args[]){
         int tab[] = {1,2,3,4,5};
         Scanner input = new Scanner(System.in);
+        int in = -1;
         System.out.println("Podal index tablicy ");
-        int in = input.nextInt();
+        in = input.nextInt();
+
         System.out.println("Index: "+tab[in]);
+
+        try{
+            System.out.println(tab[in]);
+        }
+        catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("Niepoprawny rozmiar tablicy/ za duzo "+tab.length);
+        }
 
 
     }
